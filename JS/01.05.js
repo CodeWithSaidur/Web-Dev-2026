f46ae728eeb27b864eabff0ac75fdbbfd4aafd97
CodeWithSaidur <sabedbarbhuiya3@gmail.com>
2026-01-25 18:20:41 +0530

    if else if

---
+++ JS/01.05.js
@@ -0,0 +1,30 @@
+// check Voter or Not
+let age = 17
+
+if (age >= 18) {
+  console.log('You are Voter')
+} else {
+  console.log('You are Not Voter')
+}
+
+// Get Divisition
+// 85 = D
+// 75 = S
+// 60 = 1st
+// 45 = 2nd
+// 30 = 3rd
+
+let mark = 75
+if (mark >= 85) {
+  console.log('Dist')
+} else if (mark >= 75) {
+  console.log('Star')
+} else if (mark >= 60) {
+  console.log('1st')
+} else if (mark >= 45) {
+  console.log('2nd')
+} else if (mark >= 30) {
+  console.log('3rd')
+} else {
+  console.log('Fail')
+}
