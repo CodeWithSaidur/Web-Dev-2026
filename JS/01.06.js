19b4dd52305253b4e74dce89455de9f9767a299b
CodeWithSaidur <sabedbarbhuiya3@gmail.com>
2026-01-26 19:13:32 +0530

    loop

---
+++ JS/01.06.js
@@ -0,0 +1,23 @@
+// kun kam bar bar korar lagi babohar jkora oy
+// for / while / do while / for in / for each loop
+
+// for
+// init  -> let a = 1
+// cond -> a > 10
+// update a++ or a--
+// Todo
+for (let a = 1; a <= 10; a++) {
+  console.log(a)
+}
+
+let a = 1
+let b = 20
+while (a <= b) {
+  console.log(a, '<br/>')
+  a++
+}
+
+do {
+  console.log(`${a}`)
+  a++
+} while (a <= b)
