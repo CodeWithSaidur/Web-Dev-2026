e34a2eff74de472f86b1eee22379a6c3768fdec0
CodeWithSaidur <sabedbarbhuiya3@gmail.com>
2026-02-03 19:11:53 +0530

    arr

---
+++ JS/02.09.js
@@ -0,0 +1,14 @@
+let r1 = 1
+let r2 = 2
+let r3 = 3
+let r4 = 4
+
+let students = ['Tanvir', 'Sakib', 'Arif', 'Samim', 'Rohim', 'Sahira']
+console.log(students[5])
+
+let bike = new Array('KTM', 'Palser', 'FZ', 'Supersplander', 'HERO')
+
+// bike.push('Harcolis')
+// bike.pop()
+
+console.log(bike)
