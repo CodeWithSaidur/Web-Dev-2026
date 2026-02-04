bf13663adf3b8b1aa640a827e62d1c36d03792e8
CodeWithSaidur <sabedbarbhuiya3@gmail.com>
2026-02-04 20:04:45 +0530

    Array methods

--- JS/02.09.js
+++ JS/02.09.js
@@ -12,3 +12,29 @@ let bike = new Array('KTM', 'Palser', 'FZ', 'Supersplander', 'HERO')
 // bike.pop()
 
 console.log(bike)
+
+let a = [12, 23]
+
+a.push(34) // -> Add element at the end
+a.pop() // -> Remove last element
+a.unshift(1) // -> add element at the beginning returnn length of array
+a.shift() // -> Remove first element and return it
+
+let ans1 = a.includes(23) // ->Check is elements Exist
+
+let ans2 = a.indexOf(12) // -> Get the index of the element
+
+console.log(ans1)
+
+let students1 = ['Abed', 'Tanvir', 'Salam', 'Labu', 'Sahira', 'Damand']
+
+ans = students.slice(2, 4) // Returns a copy of a section of an array.
+
+console.log(ans)
+console.log(students1)
+
+let arr3 = [10, 20, 30]
+
+arr3.splice(0, 1) // remoive selected elements change the main array
+
+console.log(arr3)
